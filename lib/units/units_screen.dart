d45f33eaca26e67c70468a451b85f5d6bbcd5f49
Ilya Buglakov <ilboogl@gmail.com>
2025-03-24 22:34:06 +0300

    feat(units): apply styling to the units menu

--- lib/units/units_screen.dart
+++ lib/units/units_screen.dart
@@ -1,6 +1,7 @@
 import 'package:fastmath/widgets/top_level_page_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 
 class UnitsScreen extends ConsumerWidget {
@@ -8,23 +9,65 @@ class UnitsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    final items = [
+      const UnitTile(
+          title: "Addition",
+          route: "/units/addition",
+          icon: FontAwesomeIcons.plus),
+      const UnitTile(
+          title: "Subtraction",
+          route: "/units/subtraction",
+          icon: FontAwesomeIcons.minus),
+      const UnitTile(
+          title: "Multiplication",
+          route: "/units/multiplication",
+          icon: FontAwesomeIcons.xmark),
+      const UnitTile(
+          title: "Division",
+          route: "/units/division",
+          icon: FontAwesomeIcons.divide),
+      const UnitTile(
+          title: "Percent calculation",
+          route: "/units/percent",
+          icon: FontAwesomeIcons.percent),
+      const UnitTile(
+          title: "Power calculation",
+          route: "/units/power",
+          icon: FontAwesomeIcons.superscript),
+    ];
+
     return TopLevelPageScaffold(
-      body: Center(
-        child: SizedBox(
-          width: 600,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            spacing: 24.0,
-            children: [
-              UnitChoiceButton("Addition", route: "/units/addition"),
-              UnitChoiceButton("Subtraction", route: "/units/subtraction"),
-              UnitChoiceButton("Multiplication", route: "/units/multiplication"),
-              UnitChoiceButton("Division", route: "/units/division"),
-              UnitChoiceButton("Percent calculation", route: "/units/percent"),
-              UnitChoiceButton("Power calculation", route: "/units/power"),
-            ],
+      body: Container(
+        color: colorScheme.surfaceContainer,
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            // decoration:
+            //     BoxDecoration(borderRadius: BorderRadius.circular(16.0)),
+            width: 600,
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.symmetric(vertical: 1.0, horizontal: 16.0),
+                  decoration: BoxDecoration(
+                    color: colorScheme.surfaceContainerLowest,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(index == 0 ? 16.0 : 4),
+                      topRight: Radius.circular(index == 0 ? 16.0 : 4),
+                      bottomLeft:
+                          Radius.circular(index == items.length - 1 ? 16.0 : 4),
+                      bottomRight:
+                          Radius.circular(index == items.length - 1 ? 16.0 : 4),
+                    ),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: items[index],
+                );
+              },
+            ),
           ),
         ),
       ),
@@ -32,20 +75,28 @@ class UnitsScreen extends ConsumerWidget {
   }
 }
 
-class UnitChoiceButton extends StatelessWidget {
-  final String text;
+class UnitTile extends StatelessWidget {
+  final IconData icon;
+  final String title;
   final String route;
 
-  const UnitChoiceButton(this.text, {super.key, required this.route});
+  const UnitTile(
+      {super.key,
+      required this.title,
+      required this.icon,
+      required this.route});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 60,
-      child: ElevatedButton(
-        onPressed: () => context.go(route),
-        child: Text(text),
+    return ListTile(
+      contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      leading: Icon(icon, size: 24.0),
+      title: Text(
+        title,
+        style: TextStyle(fontWeight: FontWeight.bold),
       ),
+      trailing: Icon(Icons.chevron_right),
+      onTap: () => context.go(route),
     );
   }
 }
